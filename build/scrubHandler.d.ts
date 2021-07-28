15004c67b61d127331a040dba303ea1caf069992
DROHEN <email@drohen.com>
2021-07-28 19:46:57 +0200

    rebuild

--- build/scrubHandler.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * - scrub event occurs
- * 	- active scrubbing occurs
- * 	- end scrub event -> lock scrubbing?
- * 	- await for "position set event"? to unlock?
- * - audio stops playing (if it was playing)
- * 	- should UI update? should it restart?
- */ 
-//# sourceMappingURL=scrubHandler.d.ts.map
